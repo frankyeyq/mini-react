33bb415e2d6005f9cf5c4b05fa288ce097a07903
frank <frankyeyq@163.com>
2024-03-25 18:37:22 +0800

    update

--- v03/demo/index.js
+++
@@ -1,9 +0,0 @@
-let taskId = 0
-function workLoop(deadLine) {
-  taskId++
-  while (!deadLine.timeRemaining() < 1) {
-    console.log('taskId', taskId);
-  }
-  requestIdleCallback(workLoop)
-}
-requestIdleCallback(workLoop)
--- v03/vite-runner/core/React.js
+++ v03/vite-runner/core/React.js
@@ -18,21 +18,78 @@ const createElement = (type, props, ...children) => {
   }
 }
 
-const render = (el, container) => {
-  const dom = el.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(el.type)
-  Object.keys(el.props).forEach(key => {
-    if (key !== 'children') {
-      dom[key] = el.props[key]
-    }
-  })
+let nextWorkUnit = null
 
-  const children = el.props.children
-  children.forEach(child => {
-    render(child, dom)
-  })
-
-  container.append(dom)
+function workLoop(deadLine) {
+  let shouldYield = false
+  while (!shouldYield && nextWorkUnit) {
+    nextWorkUnit = performWorkOfUnit(nextWorkUnit)
+    shouldYield = !deadLine.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
 }
+
+let taskId = 0
+const performWorkOfUnit = work => {
+  //1. 创建dom
+  if (!work.dom) {
+    const dom = (work.dom = 
+      work.type === 'TEXT_ELEMENT' 
+        ? document.createTextNode('') 
+        : document.createElement(work.type))
+    work.parent.dom.append(dom)
+
+    Object.keys(work.props).forEach(key => {
+      if (key !== 'children') {
+        dom[key] = work.props[key]
+      }
+    })
+  }
+  //2. 将dom结构转换成链表结构
+  const children = work.props.children
+  if (children && children.length) {
+    let prevChild = null
+    children.forEach((child, idx) => {
+      const newWork = {
+        type: child.type,
+        props: child.props,
+        dom: null,
+        child: null,
+        sibling: null,
+        parent: work
+      }
+      if (idx === 0) {
+        work.child = newWork
+      } else {
+        prevChild.sibling = newWork
+      }
+      prevChild = newWork
+    })
+
+    if (work.child) {
+      return work.child
+    }
+    if (work.sibling) {
+      return work.sibling
+    }
+    if (work.parent) {
+      return work.parent.sibling
+    }
+  }
+}
+
+const render = (el, container) => {
+  nextWorkUnit = {
+    dom: container,
+    props: {
+      children: [el]
+    }
+  }
+}
+requestIdleCallback(workLoop)
+
+
+
 
 
 const React = {
